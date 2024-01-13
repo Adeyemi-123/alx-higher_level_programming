ffcfb9958b5e6363443dc15e5d5625e32f5841ee
Adeyemi-123 <Kaluokoro608@gmail.com>
2024-01-12 23:42:10 -0800

    TRied it once with help

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    common_set = set()
+
+    for x in set_1:
+
+        if x in set_2:
+
+            common_set.add(x)
+
+    return (common_set)
